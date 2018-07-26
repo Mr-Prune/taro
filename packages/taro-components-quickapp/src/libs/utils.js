7706306e2fc2ad72fb378cd4a9373d040bc01de0
liliangquan <liliangquan@jd.com>
2018-07-26 14:24:47 +0800

    feat: update taro-quoickapp

--- packages/taro-components-quickapp/src/libs/utils.js
+++ packages/taro-components-quickapp/src/libs/utils.js
@@ -1,7 +1,8 @@
 const mergeProps = function (target) {
-  if (target._options.props && target._options.props.length) {
+  const props = typeof target._options.props === 'object' ? Object.keys(target._options.props) : target._options.props
+  if (props && props.length) {
     const { propsDefault } = target._options
-    target._options.props.forEach(prop => {
+    props.forEach(prop => {
       if (propsDefault.hasOwnProperty(prop)) {
         target[prop] = target[prop] || propsDefault[prop]
       }
